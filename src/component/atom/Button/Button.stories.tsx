a0cd75510bf0e93ec79f3b3036b43669ad0e5cd6
{tayoshiieee} <{peanutspotatopudding.gmail.com}>
2022-07-12 20:18:40 +0900

    Button の追加

--- src/component/atom/Button/Button.stories.tsx
+++ src/component/atom/Button/Button.stories.tsx
@@ -18,17 +18,7 @@ Orange.args = {
   children: <button className='btn-orange btn-base'>btn-orange</button>,
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  children: <button className='btn-sm'>btn-sm</button>,
-};
-
-export const Base = Template.bind({});
-Base.args = {
-  children: <button className='btn-base'>btn-base</button>,
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  children: <button className='btn-lg'>btn-lg</button>,
+export const Pink = Template.bind({});
+Pink.args = {
+  children: <button className='btn-pink btn-base'>btn-pink</button>,
 };
